3bb2f3ecc38bbb5024e7cf0f39266e16fb9ce539
Mariana-Usuga <marianausugamontoya12344@gmail.com>
2022-12-12 13:25:53 -0500

    subir api de propuestas

---
+++ src/test/java/com/itteamcorp/proposalControlV1/ProposalControlV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itteamcorp.proposalControlV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalControlV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
